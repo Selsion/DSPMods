4e260ea66782e8b6be0d93595e7bda1ca6eb3e92
Selsion <>
2021-07-04 18:04:26 -0400

    updated mod version info

--- DSPOptimizations/DSPOptimizations.cs
+++ DSPOptimizations/DSPOptimizations.cs
@@ -12,7 +12,7 @@ namespace DSPOptimizations
     {
         public const string MOD_GUID = "com.Selsion.DSPOptimizations";
         public const string MOD_NAME = "DSPOptimizations";
-        public const string MOD_VERSION = "0.0.1.0";
+        public const string MOD_VERSION = "0.1.0";
 
         internal void Awake()
         {
