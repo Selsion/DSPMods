6606f798a0a9e0754bd2ac4f13d55272405d2ebf
Selsion <>
2021-09-16 11:35:34 -0400

    added SaveMigrator mod

---
+++ SaveMigrator/README.md
@@ -0,0 +1,16 @@
+﻿
+# SaveMigrator Mod
+This mod was made to update save data after new updates were not reflected in old saves. Currently, it supports updating gas planets to their new types. You can use it to get the new gas giant type, which would otherwise require making a new game.
+
+## Installation
+This mod does not yet have a Thunderstore release, but it will be added once more people have tested it and confirmed that it's working correctly.
+### Manual Installation
+This mod requires BepInEx to be installed in order to work, so install that first if you haven't already. Download the latest dll for the SaveMigrator mod at the [releases](https://github.com/Selsion/DSPMods/releases) page. You will need to put this dll in the `BepInEx\plugins` directory. If you installed BepInEx manually, then this should be found in the game's steam directory. If you're using r2modman, then the path can be found by navigating to the settings page in r2modman and clicking on "Browse profile folder". Alternatively, the path can be found at `%appdata%\r2modmanPlus-local\DysonSphereProgram\profiles\[PROFILE_NAME]\BepInEx\plugins`, where `[PROFILE_NAME]` is replaced with the name you set when you created the profile.
+## Usage
+The mod will run immediately after a save is loaded, and will regenerate the planet types for each gas planet. The updated data will be stored in memory, and will not be saved to disk until you save the game either manually or with autosave. You don't need to leave the mod installed after you save the updated data to disk, and it should be uninstalled afterwards in case of incompatibility with future DSP updates.
+
+It's recommended that you carefully check that the mod behaved as expected before saving, in case of bugs. This means checking that:
+ 1. All gas planets have been changed to the new expected type
+ 2. The appearances for the gas planets are correct (e.g. no planet has the appearance of a different type)
+
+If you want to be extra careful, consider saving the game under a new name rather than overwriting the old save.
---
+++ SaveMigrator/SaveMigrator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using UnityEngine;
+using HarmonyLib;
+
+namespace SaveMigrator
+{
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    [BepInProcess("DSPGAME.exe")]
+    public class SaveMigrator : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "com.Selsion.SaveMigrator";
+        public const string MOD_NAME = "SaveMigrator";
+        public const string MOD_VERSION = "1.0";
+        internal void Awake()
+        {
+            var harmony = new Harmony(MOD_GUID);
+            harmony.PatchAll(typeof(Patch));
+        }
+
+        [HarmonyPatch]
+        public class Patch
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(GameSave), "LoadCurrentGame")]
+            public static void LoadCurrentGamePostfix(string saveName)
+            {
+                GameData data = GameMain.data;
+
+                // update the theme ID list
+                ThemeProtoSet themes = LDB.themes;
+                int num_themes = themes.Length;
+                int[] themeIds = new int[num_themes];
+                for (int i = 0; i < num_themes; i++)
+                    themeIds[i] = themes.dataArray[i].ID;
+                data.gameDesc.savedThemeIds = themeIds;
+
+                // call update func for all gas planets
+                GalaxyData galaxy = data.galaxy;
+                for(int i = 0; i < galaxy.starCount; i++)
+                {
+                    StarData star = galaxy.stars[i];
+                    for(int j = 0; j < star.planetCount; j++)
+                    {
+                        PlanetData planet = star.planets[j];
+                        if (planet.type == EPlanetType.Gas)
+                            updatePlanet(planet, themeIds);
+                    }
+                }
+            }
+
+            private static void updatePlanet(PlanetData planet, int[] themeIds)
+            {
+                // get the 5 random numbers made in CreatePlanet needed for SetPlanetTheme
+                DotNet35Random rand = new DotNet35Random(planet.infoSeed);
+                for(int i = 0; i < 12; i++)
+                    rand.NextDouble();
+                double rand1 = rand.NextDouble();
+                rand.NextDouble();
+                double rand2 = rand.NextDouble();
+                double rand3 = rand.NextDouble();
+                double rand4 = rand.NextDouble();
+                int theme_seed = rand.Next();
+                PlanetGen.SetPlanetTheme(planet, themeIds, rand1, rand2, rand3, rand4, theme_seed);
+            }
+        }
+    }
+}
