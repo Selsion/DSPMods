0cc649ff1b08cdaba5d0db46d0cecd3ec23b9e1d
Selsion <>
2021-10-01 16:41:29 -0400

    added AbnormalityParser mod

---
+++ AbnormalityParser/AbnormalityParser README.md
@@ -0,0 +1,17 @@
+﻿
+
+# AbnormalityParser Mod
+This mod can check the reasons why a save was flagged as abnormal and had achievements disabled.
+
+## Installation
+You will need to install this mod manually.
+### Manual Installation
+This mod requires BepInEx to be installed in order to work, so install that first if you haven't already. Download the latest dll for the SwarmDataWipe mod at the [releases](https://github.com/Selsion/DSPMods/releases) page. You will need to put this dll in the `BepInEx\plugins` directory. If you installed BepInEx manually, then this should be found in the game's steam directory. If you're using r2modman, then the path can be found by navigating to the settings page and clicking on "Browse profile folder". Alternatively, the path can be found at `%appdata%\r2modmanPlus-local\DysonSphereProgram\profiles\[PROFILE_NAME]\BepInEx\plugins`, where `[PROFILE_NAME]` is replaced with the name you set when you created the profile.
+## Usage
+Load a save, then open the [developer console](https://dsp-wiki.com/Developer_Console) and enter the following command:
+`-checkMask`
+
+As of version 0.8.22.8915, this command does not disable achievements.
+
+## Bug Reports
+If you have any bugs or issues to report, then either contact me on discord at Selsion#0769, or raise an issue on this github page.
---
+++ AbnormalityParser/AbnormalityParser.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using BepInEx;
+
+namespace AbnormalityParser
+{
+    [BepInPlugin(MOD_GUID, MOD_NAME, MOD_VERSION)]
+    [BepInProcess("DSPGAME.exe")]
+    public class AbnormalityParser : BaseUnityPlugin
+    {
+        public const string MOD_GUID = "com.Selsion.AbnormalityParser";
+        public const string MOD_NAME = "AbnormalityParser";
+        public const string MOD_VERSION = "1.0.0";
+
+        private static string[] bit_names = { "PROTO", "TECH", "RECIPE", "STORAGE", "CONSOLE", "MECHA", "DYSON SPHERE", "PRODUCTION", "GALAXY", "FACTORY", "NUM" };
+
+        internal void Awake()
+        {
+            var harmony = new Harmony(MOD_GUID);
+            harmony.PatchAll(typeof(Patch));
+        }
+
+        private static bool IsBitSet(int bit)
+        {
+            return (GameMain.abnormalityCheck.checkMask & 1 << bit) != 0;
+        }
+
+        private static string CheckMask(string param)
+        {
+            int count = 0;
+            string ret = "";
+            for (int i = 0; i < bit_names.Length; i++)
+            {
+                if (IsBitSet(i))
+                {
+                    ret = ret + "\n" + bit_names[i];
+                    count++;
+                }
+            }
+            return count + " abnormality type" + (count != 1 ? "s" : "") + " detected" + (count > 0 ? ":" : "") + ret;
+        }
+
+        [HarmonyPatch]
+        public class Patch
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(XConsole), "RegisterCommands")]
+            public static void RegisterCommandsPostfix()
+            {
+                XConsole.RegisterCommand("checkMask", new XConsole.DCommandFunc(CheckMask));
+            }
+        }
+    }
+}
