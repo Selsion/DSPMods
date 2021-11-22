f27a1bbc34e6e0dd23ff1a4efb1b71c27494228d
Selsion <>
2021-11-22 13:19:50 -0500

    updated AbnormalityParser for new game version

--- AbnormalityParser/AbnormalityParser.cs
+++ AbnormalityParser/AbnormalityParser.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using HarmonyLib;
 using BepInEx;
+using System.IO;
 
 namespace AbnormalityParser
 {
@@ -14,9 +15,10 @@ namespace AbnormalityParser
     {
         public const string MOD_GUID = "com.Selsion.AbnormalityParser";
         public const string MOD_NAME = "AbnormalityParser";
-        public const string MOD_VERSION = "1.1.0";
+        public const string MOD_VERSION = "1.2.0";
 
-        private static string[] bit_names = { "PROTO", "TECH", "RECIPE", "STORAGE", "CONSOLE", "MECHA", "DYSON SPHERE", "PRODUCTION", "GALAXY", "FACTORY", "NUM" };
+        public const string DIRECTORY_NAME = "AbnormalityParserMod";
+        public const string DEFAULT_FILENAME = "abnormality_info.txt";
 
         internal void Awake()
         {
@@ -24,30 +26,104 @@ namespace AbnormalityParser
             harmony.PatchAll(typeof(Patch));
         }
 
-        private static bool IsBitSet(int bit)
+        private static string CheckAbnormalities(string param)
         {
-            return (GameMain.abnormalityCheck.checkMask & 1 << bit) != 0;
-        }
-
-        private static string CheckMask(string param)
-        {
-            int count = 0;
+            GameAbnormalityData data = GameMain.data.gameAbnormality;
+            int MAX_DATA_COUNT = GameAbnormalityData.MAX_DATA_COUNT;
+            
+            int num_detected = 0;
             string ret = "";
-            for (int i = 0; i < bit_names.Length; i++)
+
+            for (int i = MAX_DATA_COUNT; i < data.runtimeDatas.Length; i += MAX_DATA_COUNT)
             {
-                if (IsBitSet(i))
+                int protoId = i / MAX_DATA_COUNT;
+                if (data.runtimeDatas[i].protoId == protoId)
                 {
-                    ret = ret + "\n" + bit_names[i];
-                    count++;
+                    num_detected++;
+                    ret = ret + LDB.abnormalities.Select(protoId).DeterminatorName + "\n";
                 }
             }
-            return count + " abnormality type" + (count != 1 ? "s" : "") + " detected" + (count > 0 ? ":" : "") + ret;
+
+            return num_detected + " abnormality type" + (num_detected == 1 ? "" : "s") + " detected" + (num_detected > 0 ? ":\n" : ".") + ret;
         }
 
-        private static string ResetMask(string param)
+        private static string WriteStringsToFile(List<string> output, string filename)
         {
-            GameMain.abnormalityCheck.checkMask = 0;
-            return "Set mask to 0";
+            // create file directory if needed
+            string file_dir = new StringBuilder(GameConfig.gameDocumentFolder).Append(DIRECTORY_NAME + "/").ToString();
+            if (!Directory.Exists(file_dir))
+            {
+                UnityEngine.Debug.Log("creating directory " + file_dir);
+                Directory.CreateDirectory(file_dir);
+            }
+
+            // open file stream
+            MemoryStream memory_stream = new MemoryStream();
+            StreamWriter w = new StreamWriter((Stream)memory_stream);
+
+            // write data
+            output.ForEach(s =>
+            {
+                w.Write(s);
+            });
+
+            // close file stream
+            w.Flush();
+            if (memory_stream != null && memory_stream.Length > 0)
+            {
+                string path = file_dir + filename;
+                UnityEngine.Debug.Log("writing to file " + path);
+                using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.Write))
+                {
+                    memory_stream.WriteTo(fileStream);
+                    fileStream.Flush();
+                    fileStream.Close();
+                    w.Close();
+                    memory_stream.Close();
+                    memory_stream = null;
+                }
+                return path;
+            }
+            else
+                throw new Exception("Unable to write file");
+        }
+
+        private static string DumpAbnormalityInfo(string param)
+        {
+            string[] param_list = param.Split(' ');
+            if (param_list.Length > 1)
+                return "invalid params";
+
+            AbnormalityRuntimeData[] runtimeDatas = GameMain.data.gameAbnormality.runtimeDatas;
+            int MAX_DATA_COUNT = GameAbnormalityData.MAX_DATA_COUNT;
+
+            List<string> output = new List<string>();
+
+            for(int i = MAX_DATA_COUNT; i < runtimeDatas.Length; i++)
+            {
+                AbnormalityRuntimeData data = runtimeDatas[i];
+                if(data.protoId == i / MAX_DATA_COUNT)
+                {
+                    string name = LDB.abnormalities.Select(data.protoId).DeterminatorName;
+                    output.Add("Found abnormality " + name + " at index " + i + ". Message:\n" + data.ToString() + "\n\n");
+                }
+            }
+
+            if (output.Count == 0)
+                return "No abnormality types were detected. No data written to disk.";
+            else
+            {
+                string filename = param == "" ? DEFAULT_FILENAME : param;
+                string filepath = WriteStringsToFile(output, filename);
+                return "Dumped abnormality data in " + filepath;
+            }
+        }
+
+        private static string ClearAbnormalities(string param)
+        {
+            AbnormalityRuntimeData[] runtimeData = GameMain.data.gameAbnormality.runtimeDatas;
+            Array.Clear(runtimeData, 0, runtimeData.Length);
+            return "Cleared abnormality data";
         }
 
         [HarmonyPatch]
@@ -57,8 +133,9 @@ namespace AbnormalityParser
             [HarmonyPatch(typeof(XConsole), "RegisterCommands")]
             public static void RegisterCommandsPostfix()
             {
-                XConsole.RegisterCommand("checkMask", new XConsole.DCommandFunc(CheckMask));
-                XConsole.RegisterCommand("resetMask", new XConsole.DCommandFunc(ResetMask));
+                XConsole.RegisterCommand("checkAbnormalities", new XConsole.DCommandFunc(CheckAbnormalities));
+                XConsole.RegisterCommand("dumpAbnormalityInfo", new XConsole.DCommandFunc(DumpAbnormalityInfo));
+                XConsole.RegisterCommand("clearAbnormalities", new XConsole.DCommandFunc(ClearAbnormalities));
             }
         }
     }
--- AbnormalityParser/README.md
+++ AbnormalityParser/README.md
@@ -9,10 +9,13 @@ You will need to install this mod manually.
 This mod requires BepInEx to be installed in order to work, so install that first if you haven't already. Download the latest dll for this mod at the [releases](https://github.com/Selsion/DSPMods/releases) page. You will need to put this dll in the `BepInEx\plugins` directory. If you installed BepInEx manually, then this should be found in the game's steam directory. If you're using r2modman, then the path can be found by navigating to the settings page and clicking on "Browse profile folder". Alternatively, the path can be found at `%appdata%\r2modmanPlus-local\DysonSphereProgram\profiles\[PROFILE_NAME]\BepInEx\plugins`, where `[PROFILE_NAME]` is replaced with the name you set when you created the profile.
 ## Usage
 The following commands are available in-game through the [developer console](https://dsp-wiki.com/Developer_Console):
-- `-checkMask`: prints names for each of the abnormality types detected
-- `-resetMask`: resets all abnormality flags for the save
+- `-checkAbnormalities`: prints names for each of the abnormality types detected
+- `-dumpAbnormalityInfo [filename]`: logs detailed info about each detected abnormality to the file `AbnormalityParserMod\filename` in your game's document folder. Prints the full path of the saved file. If no filename is given, then `abnormality_info.txt` will be used.
+- `-clearAbnormalities`: clears all abnormality data for the save
 
-As of version 0.8.22.8915, these commands do not disable achievements.
+As of version 0.8.23.9832, these commands do not disable achievements.
+
+The output from the `-dumpAbnormalityInfo` command shows the messages that would have been logged in the game's output log at the time (which may have been overwritten). You may need to translate Chinese text using a tool like google translate or deepl. If you're not sure how to interpret the dumped info, you can contact me on discord at Selsion#0769, and I can take a look at the relevant code in the game to see what exactly happened. If your save was flagged as abnormal on a vanilla save, then it would be appreciated if you send me the outputted file. I may be able to track down the bug causing the issue, and report it to the game's devs.
 
 ## Bug Reports
 If you have any bugs or issues to report, then either contact me on discord at Selsion#0769, or raise an issue on this github page.
